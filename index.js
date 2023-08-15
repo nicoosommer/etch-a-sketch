4b9af7be3f317b30d18a62bf91ca6e80f8457ae0
nicoosommer <nicosomer388@gmail.com>
2023-08-15 19:42:35 -0300

    Podes elegir de cuanto por cuanto. Poco lindo visualmente

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const container = document.querySelector(".container");
+const num = prompt("Ingrese de cuanto por cuanto es la cuadricula");
+console.log(num);
+const medida = 700 / num;
+console.log(medida);
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < num * num; i++) {
   const div = document.createElement("div");
   div.className = "cuadrado";
+  div.style.height = `${medida}px`;
+  div.style.width = `${medida}px`;
   container.appendChild(div);
 }
 function func(e) {
